c8f1e12a2fbe804688715986bd39718336881694
Akshita singh <akshitasingh170208@gmail.com>
2026-01-10 12:08:30 +0530

    add

--- binary1.cpp
+++ binary1.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 #include<vector>
 using namespace std ;
-int binary (vector<int>&num ,int target ){
+int binary (vector<int>&num){
     int n = num.size();
-    int st = 0 ;
-    int end=n-1;
+    int st =1 ;
+    int end=n-2;
     while( st <=end){
         int mid =  st+ (end-st)/2;
-        if(num[mid-1]<num[mid>mid]>num[mid+1]){
+        if(  num[mid-1] < num[mid] &&  num[mid]>num[mid+1]){//mid > 0  && mid <n-1
             return mid ;
         }
         if (num[mid]>num[mid-1]){//right search
@@ -21,7 +21,6 @@ int binary (vector<int>&num ,int target ){
     return -1;
 }
 int main(){
-        vector<int>arr1= {-1,0,3,4,5,9,12};
-        int target1= 12;
-        cout<<"the index of target is :"<<binary(arr1,target1);
+        vector<int>arr1= {0,3,8,9,5,2};
+        cout<<"the index of peak is " <<binary(arr1);
 }
