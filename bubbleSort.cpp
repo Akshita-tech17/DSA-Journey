e04a17fe9dd59c407d2ae2c0e500687a75b514f3
Akshita singh <akshitasingh170208@gmail.com>
2026-01-11 18:26:29 +0530

    add

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 using namespace std;
 void bubble( int arr[] , int n) {
+    bool Swap=false;
     for ( int i = 0 ;i< n-1;i++){
         for ( int j = 0 ; j<n-1-i; j++){
             if (arr[j] >arr[j+1]){
                 swap( arr[j], arr[j+1]);
+                Swap= true;
             }
 
+        }
+        if(Swap==false){
+            return ;
         }
     }
 }
